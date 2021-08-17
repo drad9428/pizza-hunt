e27486d174766b69b2bee648d8f8784e99f87c2f
drad <robertjdstoddard@gmail.com>
2021-08-17 17:56:30 -0600

    comments

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 const{
-    getAllPizza,
+    getAllPizzas,
     getPizzaById,
     createPizza,
     updatePizza,
@@ -10,7 +10,7 @@ const{
 
 router
     .route('/')
-    .get(getAllPizza)
+    .get(getAllPizzas)
     .post(createPizza)
 
 router
